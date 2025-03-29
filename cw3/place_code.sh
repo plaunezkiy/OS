96fdcb7653940f1893e02c8d8ed174ce3278f78b
Ploven <plush337@gmail.com>
2025-03-29 21:08:26 +0000

    cw3 task1

---
+++ cw3/place_code.sh
@@ -0,0 +1,10 @@
+
+# CORE_PATH="core.c"
+# DICE_OS_HOME="/disk/scratch/operating_systems/s2150635/linux-cw1"
+WSL_OS_HOME="/home/os/linux"
+SRC_HOME="CW3_S2150635"
+
+cp -a $SRC_HOME/fs/. $WSL_OS_HOME/fs/
+# cp -a $SRC_HOME/Task2/. $WSL_OS_HOME/
+# cp array.c $WSL_OS_HOME/fs/proc/array.c
+# cp CW1_S2150635/kernel/sched/core.c /disk/scratch/operating_systems/s2150635/linux-cw1/kernel/sched/core.c
