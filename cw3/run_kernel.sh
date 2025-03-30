ecda3227e690571b446765ead168a91f5d1cba44
Ploven <plush337@gmail.com>
2025-03-30 23:58:01 +0100

    cw3 task2

--- cw3/run_kernel.sh
+++ cw3/run_kernel.sh
@@ -1,4 +1,5 @@
 qemu-system-x86_64 -m 4G -smp 4 -drive file=/home/os/debian.qcow2 \
  -kernel /home/os/linux/arch/x86/boot/bzImage \
  -append 'root=/dev/sda1 console=ttyS0 nokaslr' \
- -nographic -S -gdb tcp::1104
+ -nographic \
+ -S -gdb tcp::1104
